e5c4dcc9a809b9b969c76f6d5750a7a535ed5fba
Mohara88s <mohara88s@gmail.com>
2021-08-03 19:42:20 +0300

    made Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
-class Button extends Component {
-  componentDidMount() {
+function Button({ onClick }) {
+  useEffect(() => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: 'smooth',
     });
-  }
-  render() {
-    const { onClick } = this.props;
-    return (
-      <button type="button" onClick={onClick} className={styles.Button}>
-        Load more
-      </button>
-    );
-  }
+  });
+  return (
+    <button type="button" onClick={onClick} className={styles.Button}>
+      Load more
+    </button>
+  );
 }
+
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
 };
